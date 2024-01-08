1c22a76f6d38286b94c669009c909a9c5a786829
lenesus <artemermacenkov24@gmail.com>
2024-01-08 20:26:23 +0200

    password encryption and pulling user data from the database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import { validationResult } from "express-validator";
 import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
 
  mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.cyuh71l.mongodb.net/?retryWrites=true&w=majority')
      .then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
@@ -11,15 +13,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
    }
 
-   res.json({
-    success: true,
+   const password = req.body.password;
+   const salt = await bcrypt.genSalt(10);
+   const passwordHash = await bcrypt.hash(password, salt);
+
+   const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
    });
+
+   const user = await doc.save();
+
+   res.json(user);
 });
 
 app.listen(4444, (err) => {
