b837e98b9024ed733215c6db0ef3075053b2e603
lenesus <artemermacenkov24@gmail.com>
2024-01-10 18:21:38 +0200

    validate errors + connect gui of data base

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { validationResult } from "express-validator";
 import { registerValidation } from './validations/auth.js';
 import UserModel from './models/User.js';
 
- mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.cyuh71l.mongodb.net/?retryWrites=true&w=majority')
+ mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.cyuh71l.mongodb.net/blog?retryWrites=true&w=majority')
      .then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
 
 const app = express();
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
@@ -32,7 +33,27 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
    const user = await doc.save();
 
-   res.json(user);
+   const token = jwt.sign(
+      {
+      _id: user._id,
+      },
+
+      'secret123',
+      {
+         expiresIn: '30d',
+      }
+   );
+
+   res.json({
+      ... user._doc,
+      token,
+   });
+  } 
+      catch (err) {
+         res.status(500).json({
+            message: 'Не удалось зарегистрироваться',
+         });
+  }
 });
 
 app.listen(4444, (err) => {
