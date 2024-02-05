b923734072a29dbcc47ed3f3b109e2d2629f692a
lenesus <artemermacenkov24@gmail.com>
2024-02-05 12:39:45 +0200

    create a general functional to create a post

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation } from './validations/auth.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controllers/UserController.js';
+
+import * as PostController from './controllers/PostController.js';
 
  mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.cyuh71l.mongodb.net/blog?retryWrites=true&w=majority')
      .then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
@@ -12,11 +14,14 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', UserController.login);
+app.post('/auth/login', loginValidation, UserController.login);
 
 app.post('/auth/register', registerValidation, UserController.register );
 
 app.get('/auth/me', checkAuth, UserController.authMe );
+
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 
 app.listen(4444, (err) => {
    if (err) {
