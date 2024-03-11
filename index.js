0ab7254e4f7b81a511b74641f057f57478f5e6b2
lenesus <artemermacenkov24@gmail.com>
2024-03-11 23:19:25 +0200

    create middleware

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
 
 import * as PostController from './controllers/PostController.js';
+import handleValidationsErrors from './utils/handleValidationsErrors.js';
 
  mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.cyuh71l.mongodb.net/blog?retryWrites=true&w=majority')
      .then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
@@ -29,9 +30,9 @@ const upload = multer({ storage });
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', loginValidation, UserController.login);
+app.post('/auth/login',loginValidation, handleValidationsErrors,  UserController.login);
 
-app.post('/auth/register', registerValidation, UserController.register );
+app.post('/auth/register', registerValidation, handleValidationsErrors, UserController.register );
 
 app.get('/auth/me', checkAuth, UserController.authMe );
 
@@ -45,7 +46,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
  app.get('/posts/:id', checkAuth, PostController.getOne);
  app.post('/posts', checkAuth, postCreateValidation, PostController.create);
  app.delete('/posts:id', checkAuth, PostController.remove);
- app.patch('/posts/:id', checkAuth, PostController.update);
+ app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 
 app.listen(4444, (err) => {
    if (err) {
