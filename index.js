03e41235971e3851214e1e6cd903be715590de26
lenesus <artemermacenkov24@gmail.com>
2024-03-11 23:00:28 +0200

    get back static files

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, UserController.login);
 
